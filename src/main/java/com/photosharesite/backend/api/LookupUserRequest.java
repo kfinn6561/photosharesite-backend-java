cf88e72dacabdd48a5081be08402d2ae9e6d62c7
Kieran Finn <kieran.finn@skyscanner.net>
2023-05-19 16:53:36 +0100

    removed some unnecessary decoration

--- src/main/java/com/photosharesite/backend/api/LookupUserRequest.java
+++ src/main/java/com/photosharesite/backend/api/LookupUserRequest.java
@@ -1,6 +1,5 @@
 package com.photosharesite.backend.api;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,10 +7,8 @@ import lombok.Setter;
 public class LookupUserRequest {
     @Getter
     @Setter
-    @JsonProperty("IPAddress")
     private String IPAddress;
 
-    @JsonCreator
     public LookupUserRequest(@JsonProperty("IPAddress") String IPAddress) {
         this.IPAddress = IPAddress;
     }
