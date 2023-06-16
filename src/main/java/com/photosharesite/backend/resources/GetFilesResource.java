07b065daa79c4c82c0dceaa21da84f2027b27076
Kieran Finn <kieran.finn@skyscanner.net>
2023-06-16 16:15:40 +0100

    changed to POST

--- src/main/java/com/photosharesite/backend/resources/GetFilesResource.java
+++ src/main/java/com/photosharesite/backend/resources/GetFilesResource.java
@@ -10,7 +10,7 @@ import org.jdbi.v3.core.Jdbi;
 
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,7 +28,7 @@ public class GetFilesResource {
         this.jdbi=jdbi;
     }
 
-    @GET
+    @POST
     @ApiOperation(value = "Get All Files", response = GetFilesResponse.class)
     @Consumes(MediaType.APPLICATION_JSON)
     @Timed
