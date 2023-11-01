0ca26b907d4728475a6e169fcde239e06e19b7a6
Kieran Finn <kieran.finn@skyscanner.net>
2023-11-01 15:03:57 +0000

    removed setter from GetFilesRequest

--- src/main/java/com/photosharesite/backend/endpoints/getfiles/GetFilesRequest.java
+++ src/main/java/com/photosharesite/backend/endpoints/getfiles/GetFilesRequest.java
@@ -2,12 +2,10 @@ package com.photosharesite.backend.endpoints.getfiles;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class GetFilesRequest {
-    private String IPAddress;
+    private final String IPAddress;
 
     public GetFilesRequest(@JsonProperty("IPAddress") String IPAddress) {
         this.IPAddress = IPAddress;
