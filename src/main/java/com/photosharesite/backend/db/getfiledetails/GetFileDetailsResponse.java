da82bc8191430e2009a6b187b8bc9ae974de6631
Kieran Finn <kieran.finn@skyscanner.net>
2023-11-08 17:21:44 +0000

    filled in details of GetFileDetailsResponse

--- src/main/java/com/photosharesite/backend/db/getfiledetails/GetFileDetailsResponse.java
+++ src/main/java/com/photosharesite/backend/db/getfiledetails/GetFileDetailsResponse.java
@@ -1,4 +1,22 @@
 package com.photosharesite.backend.db.getfiledetails;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GetFileDetailsResponse {
+    private String URL;
+    private String FileName;
+    private int OwnerID;
+
+    public GetFileDetailsResponse() {
+        // Jackson deserialisation
+    }
+
+    public GetFileDetailsResponse(String URL, String fileName, int ownerID) {
+        this.URL = URL;
+        FileName = fileName;
+        OwnerID = ownerID;
+    }
 }
