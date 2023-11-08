04bc375c9930176c32a906dbd87984577616f00d
Kieran Finn <kieran.finn@skyscanner.net>
2023-11-08 17:44:43 +0000

    add DAOs to DeleteFileResource

--- src/main/java/com/photosharesite/backend/endpoints/deletefile/DeleteFileResource.java
+++ src/main/java/com/photosharesite/backend/endpoints/deletefile/DeleteFileResource.java
@@ -2,6 +2,8 @@ package com.photosharesite.backend.endpoints.deletefile;
 
 import com.codahale.metrics.annotation.Timed;
 import com.photosharesite.backend.db.deletefile.DeleteFileAccess;
+import com.photosharesite.backend.db.getfiledetails.GetFileDetailsAccess;
+import com.photosharesite.backend.db.insertorselectuser.InsertOrSelectUserAccess;
 import com.photosharesite.backend.filemanipulation.FileDeleter;
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -15,11 +17,15 @@ import javax.ws.rs.core.MediaType;
 @Path("/files/delete")
 @Produces(MediaType.APPLICATION_JSON)
 public class DeleteFileResource {
-    private final DeleteFileAccess dao;
+    private final DeleteFileAccess deleteFileDAO;
+    private final GetFileDetailsAccess getFileDetailsDAO;
+    private final InsertOrSelectUserAccess selectUserDAO;
     private final FileDeleter fileDeleter;
 
-    public DeleteFileResource(DeleteFileAccess dao, FileDeleter fileDeleter) {
-        this.dao = dao;
+    public DeleteFileResource(DeleteFileAccess deleteFileDAO, GetFileDetailsAccess getFileDetailsDAO, InsertOrSelectUserAccess selectUserDAO, FileDeleter fileDeleter) {
+        this.deleteFileDAO = deleteFileDAO;
+        this.getFileDetailsDAO = getFileDetailsDAO;
+        this.selectUserDAO = selectUserDAO;
         this.fileDeleter = fileDeleter;
     }
 
